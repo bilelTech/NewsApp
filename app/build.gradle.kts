5f7f810c2a9295a7a29caed38d31f2cadb94360b
Bilel Najjar <bilel.najjar2013@gmail.com>
2024-12-05 23:04:27 +0100

    clean

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,10 +67,8 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    //implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.compose.navigation)
-//    implementation(libs.compose.constraint)
 
     // Paging compose
     implementation(libs.paging.compose)
